a0360ad77dd67aea421630dc4035cd46cedd4b21
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-17 17:04:02 +0300

    quick fix

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/bean/BeanConfig.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/bean/BeanConfig.java
@@ -1,8 +1,12 @@
 package com.planetsystems.tela.api.ClockInOutProducer.bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
 
 @Configuration
 public class BeanConfig {
@@ -11,5 +15,6 @@ public class BeanConfig {
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper();
 	}
+
 
 }
