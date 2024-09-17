a01acff939518e45a5aa22d121101f0795771a65
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-17 17:04:40 +0300

    tested data

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/ClockInOutProducerApplication.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/ClockInOutProducerApplication.java
@@ -1,12 +1,79 @@
 package com.planetsystems.tela.api.ClockInOutProducer;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.planetsystems.tela.api.ClockInOutProducer.dto.ClockInRequest;
+import com.planetsystems.tela.api.ClockInOutProducer.service.ClockInOutProducerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.connection.SingleConnectionFactory;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @SpringBootApplication
-public class ClockInOutProducerApplication {
+@Slf4j
+@RequiredArgsConstructor
+public class ClockInOutProducerApplication implements CommandLineRunner {
+	@Value("${queue.clockins}")
+	private String clockInsQueue;
+
+	@Value("${queue.clockin}")
+	private  String clockInQueue;
+	final ClockInOutProducerService clockInOutProducerService;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ClockInOutProducerApplication.class, args);
 	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+
+//
+		ClockInRequest request = ClockInRequest.builder()
+				.academicTermId("8a8089b191dfdd700191dfe76bc80018")
+				.clockInDateTime("12/09/2024 16:28:42")
+				.clockInType("Pin")
+				.displacement(1)
+				.telaSchoolNumber("8008221080822")
+				.staffId("8a8089108e413138018e42b383ed7972")
+				.latitude("0.3277858")
+				.longitude("32.6128736")
+				.build();
+
+
+		ClockInRequest request2 = ClockInRequest.builder()
+				.academicTermId("8a8089b191dfdd700191dfe76bc80018")
+				.clockInDateTime("12/09/2024 16:28:42")
+				.clockInType("Pin")
+				.displacement(1)
+				.telaSchoolNumber("8008221080822")
+				.staffId("8a8089108e413138018e42b383ed7972")
+				.latitude("0.3277858")
+				.longitude("32.6128736")
+				.build();
+
+//		List.of(1,2,2,3,4,5,6).forEach(integer -> {
+//			clockInOutProducerService.publishClockIns(request);
+//			clockInOutProducerService.publishClockIns(request2);
+//			log.info("PRODUCER "+integer);
+//		});
+
+	}
+
+
 
 }
