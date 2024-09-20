4911dadb92b75bc42a79b9e0a1ae052dcdc81294
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-09-20 17:59:56 +0300

    added fromString

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/dto/RequestType.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/dto/RequestType.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Optional;
 
 @AllArgsConstructor
 @Getter
@@ -11,4 +13,10 @@ public enum RequestType implements Serializable {
     SCHOOL("SCHOOL"),CLASSES("CLASSES"),STAFFS("STAFFS"),CLOCKINS("CLOCKINS"), CLOCKIN("CLOCKIN"), SUBJECTS("SUBJECTS"), LEARNER_HEADCOUNTS("LEARNER_HEADCOUNTS"), LEARNER_ATTENDANCES("LEARNER_ATTENDANCES"), DISTRICTS("DISTRICTS");
 
         String type;
+
+        public static Optional<RequestType> fromString(String requestType){
+        return Arrays.stream(RequestType.values()).filter(re -> requestType.equalsIgnoreCase(re.type)).findFirst();
+        }
+
+
 }
