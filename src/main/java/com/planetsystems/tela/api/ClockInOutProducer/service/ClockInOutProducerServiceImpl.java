4e3697f9de92dd6073e38c21995d1a8ecdf1f6bb
fkasoma <147488985+fkasoma@users.noreply.github.com>
2024-11-11 16:39:39 +0300

    Update ClockInOutProducerServiceImpl.java

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/service/ClockInOutProducerServiceImpl.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/service/ClockInOutProducerServiceImpl.java
@@ -14,6 +14,7 @@ import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 import java.util.Map;
