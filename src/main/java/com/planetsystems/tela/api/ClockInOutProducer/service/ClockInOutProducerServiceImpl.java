adbe0a7f49c1cf25b24fc4904b58315749eaefde
fkasoma <147488985+fkasoma@users.noreply.github.com>
2024-11-11 17:27:23 +0300

    Update ClockInOutProducerServiceImpl.java-MediaType

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/service/ClockInOutProducerServiceImpl.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/service/ClockInOutProducerServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
+import org.springframework.http.MediaType;
 
 import java.util.List;
 import java.util.Map;
