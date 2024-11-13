54d3c4c92c5b328042bcb0d70daea3657f17b589
galiwango.fahad@gmail.com <galiwango.fahad@gmail.com>
2024-11-13 11:20:45 +0300

    added consumerBaseUrl and Rest Bean

--- src/main/java/com/planetsystems/tela/api/ClockInOutProducer/bean/BeanConfig.java
+++ src/main/java/com/planetsystems/tela/api/ClockInOutProducer/bean/BeanConfig.java
@@ -1,20 +1,27 @@
 package com.planetsystems.tela.api.ClockInOutProducer.bean;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
-import org.springframework.jms.connection.SingleConnectionFactory;
+import org.springframework.web.client.RestClient;
 
 @Configuration
+@Slf4j
 public class BeanConfig {
+
+	@Value("${consumerBaseUrl}")
+	String consumerBaseUrl;
 
 	@Bean
 	public ObjectMapper objectMapper(){
 		return new ObjectMapper();
 	}
 
+	@Bean
+	public RestClient restClient(){
+		return RestClient.create(consumerBaseUrl);
+	}
 
 }
